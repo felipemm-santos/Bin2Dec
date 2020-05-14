7fbe50eb807e68afee6d4fca58b134af436ec123
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-05-13 23:09:13 -0300

    Funcionalidade terminada

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,17 @@
-let txtBin = document.getElementById("idBin")
+let txtBin = document.getElementById('idBin');
 
-txtBin.focus()
+txtBin.focus();
 
-function converter(){
-    window.alert("Converter")
+function converter() {
+  let bin = txtBin.value;
+  if (bin) {
+    if (RegExp('^[01]+$').test(bin)) {
+      let res = parseInt(bin, 2);
+      document.getElementById('idDec').value = res;
+    } else {
+      window.alert('O número digitado não é uum número binário');
+    }
+  } else {
+    window.alert('Nenhum número digitado');
+  }
 }
