2d6995d0c54b03e26a91092a1365cd8df62219d7
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-05-17 13:38:45 -0300

    Add style

--- javascript/script.js
+++ javascript/script.js
@@ -2,16 +2,29 @@ let txtBin = document.getElementById('idBin');
 
 txtBin.focus();
 
+txtBin.onkeydown = function (event) {
+  const e = event || window.event;
+  if (e.which == 13) {
+    converter();
+  }
+};
+
 function converter() {
   let bin = txtBin.value;
+  const txtDec = document.getElementById('idDec');
+
   if (bin) {
     if (RegExp('^[01]+$').test(bin)) {
       let res = parseInt(bin, 2);
-      document.getElementById('idDec').value = res;
+      txtDec.value = res;
     } else {
       window.alert('O número digitado não é uum número binário');
+      txtDec.value = '';
+      txtBin.value = '';
     }
   } else {
     window.alert('Nenhum número digitado');
   }
+  txtBin.focus();
+  txtBin.select()
 }
